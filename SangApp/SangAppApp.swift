9e7bda991ebdb8098df9ec7f990914c956b94251
sangtt18 <SangTt18@fpt.com.vn>
2023-09-10 22:55:40 +0700

    Initial Commit

---
+++ SangApp/SangAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SangAppApp.swift
+//  SangApp
+//
+//  Created by Sang Truong on 10/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct SangAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
